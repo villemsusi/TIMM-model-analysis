5b08f2daac756e9628a313856e36a1afb7ce4f83
villemsusi <villem.susi@gmail.com>
2023-12-08 14:22:44 +0200

    Refactored train/test split script

--- create_train_test_split.py
+++ create_train_test_split.py
@@ -4,10 +4,8 @@ import shutil
 
 random.seed(10)
 
-direc = "oxford-iiit-pet/images/"
 
-
-def splitRandom(train_size, val_size, test_size):
+def splitRandom(train_size, val_size):
     for f in os.listdir(direc):
         file = os.path.join(direc,f)
         if not os.path.isfile(file):
@@ -27,30 +25,32 @@ def splitRandom(train_size, val_size, test_size):
         else:
             shutil.move(file, direc+"test/"+f)
 
-def createSubdirectories():
-    for subdirec in ["train/", "val/", "test/"]:
-        for i, f in enumerate(os.listdir(direc+subdirec)):
-            f_abs_path = os.path.join(direc, subdirec, f)
-            if not os.path.isfile(f_abs_path):
-                continue
-            if ".mat" in f:
-                os.remove(f_abs_path)
-            label = f.rpartition("_")[0]
-            if label.startswith("train"):
-                label = label.replace("train", "")
-            elif label.startswith("validation"):
-                label = label.replace("validation", "")
-            elif label.startswith("test"):
-                label = label.replace("test", "")
-            if not os.path.exists(direc+subdirec+label):
-                os.mkdir(direc+subdirec+label)
+def createSubdirectories(d):
 
-            shutil.move(f_abs_path, f"{direc}{subdirec}{label}/{label}_{i}.jpg")
-        else:
+    for i, f in enumerate(os.listdir(d)):
+        f_abs_path = os.path.join(d, f)
+        if not os.path.isfile(f_abs_path):
             continue
-        break
+        if ".mat" in f:
+            os.remove(f_abs_path)
+        label = f.rpartition("_")[0]
+        if label.startswith("train"):
+            label = label.replace("train", "")
+        elif label.startswith("validation"):
+            label = label.replace("validation", "")
+        elif label.startswith("test"):
+            label = label.replace("test", "")
+        if not os.path.exists(d+label):
+            os.mkdir(d+label)
+
+        shutil.move(f_abs_path, f"{d}{label}/{label}_{i}.jpg")
+
 
 if __name__ == "__main__":
+
+    direc = "oxford-iiit-pet/images/"
+
     splitRandom(0.75, 0.15, 0.1)
 
-    createSubdirectories()
+    for subdirec in ["train/", "val/", "test/"]:
+        createSubdirectories(direc+subdirec)
